cf00643b1efe465a89792aa153e5e54c73353a23
Nikhil Bansal <nikbansal1998@gmail.com>
2018-10-19 01:08:29 +0530

    Odd numbers between two intervals

--- B_S2_4.java
+++ B_S2_4.java
@@ -2,15 +2,30 @@ import java.util.*;
 import java.lang.*;
 class B_S2_4
 {
+	static void isodd(int N,int Q)
+	{
+		for(int i=N+1;i<Q;i++)
+		{
+			if(i%2!=0)
+				System.out.println(i);
+		}
+	}
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
 		int N=sc.nextInt();
 		int Q=sc.nextInt();
-		for(int i=N+1;i<=Q;i++)
-		{
-			if(i%2!=0)
-				System.out.println(i);
-		}
+		if(N<Q)
+			isodd(N,Q);
+		else if(N>Q)
+			{
+				N=N^Q;
+				Q=N^Q;
+				N=N^Q;
+				isodd(N,Q);
+			}
+			else
+				System.out.println("Please enter two different numbers");
+
 	}
 }
