07d58855d708d654cbd5aafaad5fed0f8027b36c
Nikhil Bansal <nikbansal1998@gmail.com>
2018-10-19 13:22:57 +0530

    Arithmetic progression

---
+++ B_S3_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.lang.Math;
+class B_S3_1
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=sc.nextInt();
+		int d=sc.nextInt();
+		int sum=(n)*((2*a)+(n-1)*d)/2;
+		System.out.println(sum);
+		
+	}
+}
