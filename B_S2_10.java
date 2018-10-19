825e1d705c0adc60389d34ee2de2ab36787ff9c6
Nikhil Bansal <nikbansal1998@gmail.com>
2018-10-19 11:07:51 +0530

    First five multiples of number N

---
+++ B_S2_10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.Math;
+class B_S2_10
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		for(int i=1;i<=5;i++)
+		{
+			System.out.print(num*i+" ");
+		}
+	}
+}
