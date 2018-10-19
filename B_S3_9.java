7fcc5c99c28a31f80939b35174bc6428108288c4
Nikhil Bansal <nikbansal1998@gmail.com>
2018-10-19 23:31:08 +0530

    Evaluating time in hours and minutes

--- B_S3_9.java
+++ B_S3_9.java
@@ -6,9 +6,13 @@ class B_S3_9
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter time in minutes");
 		int time_in_min=sc.nextInt();
-		int time_in_hr=time_in_min/60;
-		time_in_min=time_in_min%60;
-		System.out.println(time_in_hr+" "+time_in_min);
-		
+		if(time_in_min>=0)
+		{
+			int time_in_hr=time_in_min/60;
+			time_in_min=time_in_min%60;
+			System.out.println(time_in_hr+" "+time_in_min);
+		}
+		else
+			System.out.println("Invalid");
 	}
 }
