9ee3fb8123c55987cb28551cfa0fb1c5508a5197
Nikhil Bansal <nikbansal1998@gmail.com>
2018-10-21 23:06:52 +0530

    Print S string K times

---
+++ B_S5_1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class B_S5_1
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String S=sc.next();
+		int K=sc.nextInt();
+		for(int i=0;i<K;i++)
+		{
+			System.out.println(S);
+		}
+	}
+}
