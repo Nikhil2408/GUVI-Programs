7bebcff8441c097a7a471b01d8aacd1788e2ebd3
Nikhil Bansal <nikbansal1998@gmail.com>
2018-10-21 23:50:28 +0530

    Print the next number

---
+++ B_S5_6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class B_S5_6
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		System.out.println(N+1);
+	}
+}
