8063bd2206621dfad99c2a4449aa2eff29a8880a
Nikhil Bansal <nikbansal1998@gmail.com>
2018-10-22 00:02:35 +0530

    Print the average of N numbers

---
+++ B_S5_8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class B_S5_8
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		int sum=0;
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			sum=sum+arr[i];
+		}
+		System.out.println(sum/n);
+	}
+}
