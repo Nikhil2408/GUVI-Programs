71db787379d51d68073f1d9f201f255bb8a347ed
Nikhil Bansal <nikbansal1998@gmail.com>
2018-10-22 00:09:44 +0530

    Find whether the number is a power of 2

---
+++ B_S5_10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class B_S5_10
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		if(num%2==0)
+			System.out.println("yes");
+		else
+			System.out.println("no");
+	}
+}
