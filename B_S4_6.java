41e3f28366e8e6192a687342d33f2008ec9b4a60
Nikhil Bansal <nikbansal1998@gmail.com>
2018-10-21 22:51:48 +0530

    Counting number of special characters in a given string

--- B_S4_6.java
+++ B_S4_6.java
@@ -10,7 +10,9 @@ class B_S4_6
 		int count=0;
 		for(int i=0;i<str.length();i++)
 		{
-			if(str.charAt(i)>='0' && str.charAt(i)<='9')
+			if(str.charAt(i)>='0' && str.charAt(i)<='9' || str.charAt(i)>='A' && str.charAt(i)<='Z' || str.charAt(i)>='a' && str.charAt(i)<='z' || str.charAt(i)==' ')
+				continue;
+			else
 				count++;
 		}
 		
