f425760ffddb18b795d26bb8b8c1edabeca7214e
Nikhil Bansal <nikbansal1998@gmail.com>
2018-11-30 21:04:20 +0530

    sum of K natural numbers

---
+++ B_S6_10.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class B_S6_10
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(n*(n+1)/2);
+	}
+}
