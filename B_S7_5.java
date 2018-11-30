4164ed4d7d3d0bbf3b5a30bf4dd8b2bdf3086e61
Nikhil Bansal <nikbansal1998@gmail.com>
2018-11-30 21:56:02 +0530

    print string without using semicolon in program

---
+++ B_S7_5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class B_S7_5
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		if(System.out.printf(s)==null)
+		{
+
+		}
+		
+	}
+}
