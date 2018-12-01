cf99fe047e34964ccc3486fffc17133d3848ca04
Nikhil Bansal <nikbansal1998@gmail.com>
2018-12-01 09:47:47 +0530

    decimal number round it off to nearest greater integer

---
+++ B_S8_4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.*;
+class B_S8_4
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		float n=sc.nextFloat();
+		System.out.println((int)Math.ceil(n));
+		
+	}
+}
