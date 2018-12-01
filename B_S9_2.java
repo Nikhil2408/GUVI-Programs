b81fb877cd9634b4bc73b062ff6dd29a0d06d211
Nikhil Bansal <nikbansal1998@gmail.com>
2018-12-01 11:28:04 +0530

    area of a farm upto 5 decimal places

---
+++ B_S9_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.*;
+class B_S9_2
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		float length=sc.nextFloat();
+		float breadth=sc.nextFloat();
+		System.out.printf("%.5f",length*breadth);
+	}
+}
