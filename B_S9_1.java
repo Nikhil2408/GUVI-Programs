ab6751286d9292b7fc2565626dfa414a27fcfa19
Nikhil Bansal <nikbansal1998@gmail.com>
2018-12-01 11:23:32 +0530

    difference between kabali numbers and opponent numbers

---
+++ B_S9_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.*;
+class B_S9_1
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int kabali_num=sc.nextInt();
+		int opp_num=sc.nextInt();
+		System.out.println(Math.abs(kabali_num-opp_num));
+	}
+}
