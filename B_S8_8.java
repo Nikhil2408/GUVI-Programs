5decff2896982bef29c2bce503a2867a3b342eb4
Nikhil Bansal <nikbansal1998@gmail.com>
2018-12-01 10:34:42 +0530

    Number is a multiple of 13 or not

---
+++ B_S8_8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.math.*;
+class B_S8_8
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%13==0)
+			System.out.println("yes");
+		else
+			System.out.println("no");
+		
+	}
+}
