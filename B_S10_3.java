6acc21a70831ad02532829a64cdaf1124d31b4a6
Nikhil Bansal <nikbansal1998@gmail.com>
2019-01-11 22:28:23 +0530

    convert temp. from celcius to kelvin

---
+++ B_S10_3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.*;
+class B_S10_3
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int celcius=sc.nextInt();
+		int kelvin=celcius+273;
+		System.out.println(kelvin);
+	}
+}
