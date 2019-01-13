c0cb01a578ecb2a745c66f940e7195b8db9a8c28
Nikhil Bansal <nikbansal1998@gmail.com>
2019-01-13 15:11:05 +0530

    Given N and K, compute N to the power of K

---
+++ B_S11_4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.*;
+class B_S11_4
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int K=sc.nextInt();
+		System.out.println((int)Math.pow(N,K));
+	}
+}
