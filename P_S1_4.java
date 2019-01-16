2dc9439e832dd5db46b85ffe0a29b9f6c6490e37
Nikhil Bansal <nikbansal1998@gmail.com>
2019-01-16 22:07:58 +0530

    Given a string S, append a full stop at the end of string

---
+++ P_S1_4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+//import java.math.*;
+class P_S1_4
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String stl=sc.next();
+		StringBuilder sb=new StringBuilder(stl);
+		sb.append(".");
+		System.out.println(sb);
+	}
+}
