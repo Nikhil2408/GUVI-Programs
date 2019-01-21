289189cf9e83e9d9770788b934590301d5a89810
Nikhil Bansal <nikbansal1998@gmail.com>
2019-01-21 21:45:35 +0530

    Given a string S, print the reversed string after removing all vowels

--- P_S2_4.java
+++ P_S2_4.java
@@ -5,7 +5,10 @@ class P_S2_4
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		sc.nextLine();
 		String str=sc.nextLine();
+		str=str.substring(0,N);
 		str=str.toLowerCase();
 		StringBuilder sb=new StringBuilder();
 		for(int i=0;i<str.length();i++)
